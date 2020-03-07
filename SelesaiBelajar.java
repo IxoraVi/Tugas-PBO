5943997f8ac662d09ad29c77e7f5ac9716a13621
Amnah <59590227+IxoraVi@users.noreply.github.com>
2020-03-07 12:14:10 +0800

    Add files via upload

---
+++ SelesaiBelajar.java
@@ -0,0 +1,7 @@
+package selesai.belajar;
+public class SelesaiBelajar {
+    public static void main(String[] args) {
+        System.out.println("Selesai Belajar Pemrograman Java");
+    }
+    
+}
